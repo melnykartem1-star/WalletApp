b1e9e15040998d8dcbe003a4f11053031ed3d71f
melnykartem1-star <melnyk.a.yu.-io46@edu.kpi.ua>
2026-05-18 18:57:26 +0300

    feat: add repository annotation to user repo

--- src/main/java/org/my/walletapp/repository/user/UserRepository.java
+++ src/main/java/org/my/walletapp/repository/user/UserRepository.java
@@ -2,9 +2,11 @@ package org.my.walletapp.repository.user;
 
 import org.my.walletapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
