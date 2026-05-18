a13b1027c3557dc09355fd1d6a89c8f63ab10035
melnykartem1-star <melnyk.a.yu.-io46@edu.kpi.ua>
2026-05-18 18:55:19 +0300

    feat: add user repository

---
+++ src/main/java/org/my/walletapp/repository/user/UserRepository.java
@@ -0,0 +1,13 @@
+package org.my.walletapp.repository.user;
+
+import org.my.walletapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+}
