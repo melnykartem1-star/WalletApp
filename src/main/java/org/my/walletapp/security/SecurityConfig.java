a048d0594811d47aa835ccd8c7bd69ecc437492d
melnykartem1-star <melnyk.a.yu.-io46@edu.kpi.ua>
2026-05-19 22:11:20 +0300

    refactor: give access for Swagger

--- src/main/java/org/my/walletapp/security/SecurityConfig.java
+++ src/main/java/org/my/walletapp/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 }))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Дозволяємо Swagger
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
