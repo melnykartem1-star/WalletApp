fcd242ce22bb4c3be5a9edae88f2b0acb97d632d
melnykartem1-star <melnyk.a.yu.-io46@edu.kpi.ua>
2026-05-19 16:54:52 +0300

    feat: create transaction entity

--- src/main/java/org/my/walletapp/entity/Transaction.java
+++ src/main/java/org/my/walletapp/entity/Transaction.java
@@ -1,10 +1,62 @@
 package org.my.walletapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.my.walletapp.enums.TransactionType;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@Table(name = "transactions")
 public class Transaction {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_account_id")
+    private Account targetAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "merchant_id")
+    private Merchant merchant;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(precision = 19, scale = 4, nullable = false)
+    private BigDecimal amount;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now().withNano(0);
+        }
+    }
+
 }
