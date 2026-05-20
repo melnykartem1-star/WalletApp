db89897b4cb5045c9e0767abf18ba89864f344a3
melnykartem1-star <melnyk.a.yu.-io46@edu.kpi.ua>
2026-05-20 20:58:00 +0300

    feat: add version column to account

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE accounts(
     account_id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
     balance NUMERIC(19, 4) DEFAULT 0.0000 CHECK (balance >= 0),
+    version BIGSERIAL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     currency VARCHAR(3) NOT NULL,
